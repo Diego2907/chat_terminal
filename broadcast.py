3baae7b15a4370b34ad3e1ba431d5e6268e859cc
Lorenzo <rdzcha77@gmail.com>
2026-09-08 08:32:35 -0600

    broadcast funcional

--- broadcast.py
+++ broadcast.py
@@ -1,4 +1,9 @@
-# una lista compartida de clientes conectados, para reenviarles el mensaje a todos.
+import socket, threading
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind(("localhost", 5000))
+servidor.listen()
+print("Escuchando en el puerto 5000")# una lista compartida de clientes conectados, para reenviarles el mensaje a todos.
+
 clientes_conectados = []
 
 def atender_cliente(conexion, direccion):
@@ -17,3 +22,10 @@ def atender_cliente(conexion, direccion):
     finally:
         clientes_conectados.remove(conexion)
         conexion.close()
+        
+while True:
+    conexion, direccion = servidor.accept()
+    print(f"Nuevo Hilo {direccion}, ")
+
+    hilo = threading.Thread(target=atender_cliente, args=(conexion, direccion))
+    hilo.start()
